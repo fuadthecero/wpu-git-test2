04d80ddf40669891f1bbab76345d8fc12ef21afc
Fuad <ariefraharjo95@gmail.com>
2022-12-24 10:33:19 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, tenetur repellat temporibus illo rerum fugit? Soluta quis quibusdam a eligendi qui ipsa ut omnis architecto, eaque, eos excepturi officiis unde.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
